5db59b8dd9e12a3e5492462b308101c2dce88f92
iraholston <36212283+iraholston@users.noreply.github.com>
2019-05-30 10:37:49 -0700

    Create recfrac.js

---
+++ recfrac.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(700, 600);
+}
+function draw() {
+
+    background(0);
+  stroke(255);
+  noFill();
+  drawCircle(350,300,300);
+  noLoop();
+}
+
+function drawCircle(x,y,d) {
+  ellipse(x,y,d);
+  if (d>2 ){
+    drawCircle(x+d*0.5,y,d*0.5)
+    drawCircle(x-d*0.5,y,d*0.5)
+    drawCircle(x,y+d*0.5,d*0.5)
+    //drawCircle(x,y-d*0.5,d*0.4)
+  }
+}
